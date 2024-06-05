fa820640aedf3bf4631f766a3e243c4746257769
Lytergy0 <164952126+LuccaMahoraga10@users.noreply.github.com>
2024-06-05 16:55:17 -0300

    Update Trabalho 15.js

--- Trabalho 15.js
+++ Trabalho 15.js
@@ -16,3 +16,8 @@ function gerarCombinações(letra1, letra2, letra3) {
     
     return combinacoes;
 }
+const letra1 = 'a';
+const letra2 = 'b';
+const letra3 = 'c';
+const resultado = gerarCombinações(letra1, letra2, letra3);
+console.log("Combinações únicas:", resultado);
